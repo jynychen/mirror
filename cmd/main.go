fcb0fdfca5fc44d538452e69291739d52306847b
Jerry Y. Chen <chen@jyny.dev>
2024-01-21 00:59:50 +0800

    feat: apply pkg to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"github.com/jynychen/mirror/pkg/log"
+	"github.com/jynychen/mirror/pkg/mirror"
+)
+
 func main() {
+	logger := log.New()
+
+	srcRepoURL := ""
+	destRepoURL := ""
+
+	if err := mirror.New(srcRepoURL, destRepoURL, logger).Run(); err != nil {
+		logger.Fatalf("Mirror.Run() err: %v", err)
+	}
 }
